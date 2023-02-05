fffda486b9419cb2e34ada8913ac5e5e86596b81
mallikarjuna <mallikharjuna@divum.in>
2023-02-05 14:31:33 +0530

    google login sdk integretions

--- src/screens/screen.js
+++ src/screens/screen.js
@@ -1,8 +1,10 @@
 import * as React from 'react';
-import { View, Text, FlatList, StyleSheet, SafeAreaView } from 'react-native';
+import { View, Text, FlatList, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native';
 import Service from '../networking/service';
 import { endpoints } from '../networking/urls';
 import observable$ from '../rxjs/seviec';
+import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
+
 import App from './drag';
 const ListView = () => {
   React.useEffect(() => {
@@ -10,7 +12,46 @@ const ListView = () => {
     subscription();
   }, []);
 
+  React.useEffect(() => {
+    GoogleSignin.configure({
+     scopes: ['email'],
+     webClientId: '58346678539-ejrj6r0bkaqr33859lfkkdnpp50nqa06.apps.googleusercontent.com',
+     iosClientId: '',
+     forceCodeForRefreshToken: true,
+     offlineAccess: true,
+    });
+   }, []);
+
   const [listdata, setListData] = React.useState([]);
+
+
+  const openGoogleSignin = async () => {
+    try {
+     await GoogleSignin.hasPlayServices();
+     const userInfo = await GoogleSignin.signIn();
+     console.log(userInfo,"userInfo");
+     const _authToken = userInfo.idToken ? userInfo.idToken : '';
+     console.log('Google-Access-Token===>', _authToken);
+    //  doGoogleLogin(_authToken.toString());
+    } catch (error) {
+     if (error?.code === statusCodes.SIGN_IN_CANCELLED) {
+      // showToastInBottom(Title.LOGIN_CANCELLED);
+      console.log(statusCodes.SIGN_IN_CANCELLED,"statusCodes.SIGN_IN_CANCELLED");
+     } else if (error?.code === statusCodes.IN_PROGRESS) {
+      // showToastInBottom(Title.LOGIN_IN_PROGRESS);
+      console.log(statusCodes.IN_PROGRESS,"statusCodes.IN_PROGRESS");
+
+     } else if (error?.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+      // showToastInBottom(Title.PLAY_SERVICES_NOT_AVAILABLE);
+      console.log(statusCodes.PLAY_SERVICES_NOT_AVAILABLE,"statusCodes.PLAY_SERVICES_NOT_AVAILABLE");
+
+     } else {
+      console.log("statusCodes.SOMETHING_WENT_WRONG");
+
+      // showToastInBottom(Title.SOMETHING_WENT_WRONG);
+     }
+    }
+   };
   const getListApi = async () => {
     try {
       let response = await Service.getAnswers(endpoints.answers);
@@ -44,6 +85,14 @@ const ListView = () => {
   const renderItem = ({ item }) => <Item item={item} />;
   return (
     <SafeAreaView style={styles.container}>
+
+      <TouchableOpacity onPress={openGoogleSignin}>
+        <View>
+          <Text>
+            hi google  
+          </Text>
+        </View>
+      </TouchableOpacity>
       <App/>
       <FlatList
         data={listdata}
