7c436eb05ac0e93e1e945832162d08b8b9ffaeed
Mallikarjuna-s <78949083+Mallikarjuna-s@users.noreply.github.com>
2023-05-06 19:16:26 +0530

    added changes

--- src/coustumtabs/tabs/tabs.js
+++ src/coustumtabs/tabs/tabs.js
@@ -1,32 +1,27 @@
-import React, { FC } from "react";
-import { View,Text } from 'react-native';
-import { TouchableOpacity } from 'react-native';
+import React, {FC} from 'react'
+import {View, Text} from 'react-native'
+import {TouchableOpacity} from 'react-native'
 
-const Tabs = ({
-  tabs = [],
-  selectedTab = 0,
-  onPress,
-}) => {
-  const Panel = tabs && tabs.find((tab, index) => index === selectedTab);
+const Tabs = ({tabs = [], selectedTab = 0, onPress}) => {
+    const Panel = tabs && tabs.find((tab, index) => index === selectedTab)
 
-  return (
-    <View>
-      <View>
-        {tabs.map((tab, index) => (
-          <TouchableOpacity
-            // style={{  }}
-            onPress={() => onPress(index)}
-            key={index}
-            tabIndex={selectedTab === index ? 0 : -1}
-          >
-           <Text>{tab.label}</Text> 
-          </TouchableOpacity>
-        ))}
-      </View>
-      <View style={{flex:1}}>
-      {Panel && <Panel.Component index={selectedTab} />}
-      </View>
-    </View>
-  );
-};
-export default Tabs;
+    return (
+        <View>
+            <View>
+                {tabs.map((tab, index) => (
+                    <TouchableOpacity
+                        // style={{  }}
+                        onPress={() => onPress(index)}
+                        key={index}
+                        tabIndex={selectedTab === index ? 0 : -1}>
+                        <Text>{tab.label}</Text>
+                    </TouchableOpacity>
+                ))}
+            </View>
+            <View style={{flex: 1}}>
+                {Panel && <Panel.Component index={selectedTab} />}
+            </View>
+        </View>
+    )
+}
+export default Tabs
--- src/screens/tabs.js
+++ src/screens/tabs.js
@@ -1,33 +1,34 @@
-import React, { useState } from "react";
-import Tabs from '../coustumtabs/tabs/tabs';
-import ResponsiveDrag from './drag';
-import ListView from './screen';
+import React, {useState} from 'react'
+import Tabs from '../coustumtabs/tabs/tabs'
+import ResponsiveDrag from './drag'
+import ListView from './screen'
 // import GetLoder from './CustomLoader'
 
 const tabs = [
-  {
-    label: "Tab One",
-    index: 1,
-    Component: ResponsiveDrag
-  },
-  {
-    label: "Tab Two",
-    index: 2,
-    Component: ListView
-  },
-  // {
-  //   label: "Tab Three",
-  //   index: 3,
-  //   Component: GetLoder
-  // }
-];
+    {
+        label: 'Tab One',
+        index: 1,
+        Component: ResponsiveDrag,
+    },
+    {
+        label: 'Tab Two',
+        index: 2,
+        Component: ListView,
+    },
+    // {
+    //   label: "Tab Three",
+    //   index: 3,
+    //   Component: GetLoder
+    // }
+]
 
 const TabsRender = () => {
-  const [selectedTab, setSelectedTab] = useState(tabs[0].index);
+    const [selectedTab, setSelectedTab] = useState(tabs[0].index)
 
-  return (
-    <Tabs selectedTab={selectedTab} onPress={setSelectedTab} tabs={tabs} />
-  );
-}
+    return (
+        <Tabs selectedTab={selectedTab} onPress={setSelectedTab} tabs={tabs} />
+    )
+};
 
 export default TabsRender;
+
